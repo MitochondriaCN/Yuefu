2b0fb9729f404ed11452e595bc77f7dad2f6664a
线粒体XianlitiCN <mitochondriacn@gmail.com>
2026-03-07 16:41:04 +0800

    feat: 增加Preferences Datastore并将其注册为DI项

---
+++ app/src/main/java/com/xianliticn/yuefu/modules/SettingsModule.kt
@@ -0,0 +1,20 @@
+package com.xianliticn.yuefu.modules
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SettingsModule {
+
+    @Provides
+    @Singleton
+    fun provideSettingsManager(@ApplicationContext context: Context): SettingsManager {
+        return SettingsManager(context)
+    }
+}
