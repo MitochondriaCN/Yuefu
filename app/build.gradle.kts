d98150ada6c47ff6145543bbae76943f26398e06
线粒体XianlitiCN <mitochondriacn@gmail.com>
2026-03-16 21:46:14 +0800

    build: Bump version to v0.2.3-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 3
-        versionName = "v0.2.2-beta"
+        versionName = "v0.2.3-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
